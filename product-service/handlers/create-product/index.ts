063354cb34c377b27b8bde56b87c46df5e933f82
Andrei Sianiuta <anderysenuta@gmail.com>
2020-11-10 13:36:33 +0300

    task 4

---
+++ product-service/handlers/create-product/index.ts
@@ -0,0 +1,52 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { Client } from 'pg';
+import dbOptions from '../../utils/dbOptions';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  console.log('Create product handler: ', event);
+  let client = null;
+
+  try {
+    const { title, description, price, count } = JSON.parse(event.body);
+    if (
+      typeof title !== "string" ||
+      typeof description !== "string" ||
+      typeof price !== "number" ||
+      typeof count !== "number"
+    ) {
+      throw { status: 400, message: 'Product data is invalid' };
+    }
+
+    client = new Client(dbOptions);
+    await client.connect();
+
+    await client.query('BEGIN');
+    const query = {
+      text: 'INSERT INTO products(title, description, price) VALUES($1, $2, $3) RETURNING id',
+      values: [title, description, price]
+    };
+    const { rows } = await client.query(query)
+    const insertPhotoText = 'INSERT INTO stocks(product_id, count) VALUES ($1, $2)'
+    const insertPhotoValues = [rows[0].id, count]
+    await client.query(insertPhotoText, insertPhotoValues)
+    await client.query('COMMIT')
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(rows[0])
+    };
+  }catch (error) {
+    console.error('Create product error:', error.message);
+    if (client) {
+      await client.query('ROLLBACK')
+    }
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify(error.message),
+    };
+  }finally {
+    if (client) {
+      client.end()
+    }
+  }
+}
--- product-service/handlers/get-product-by-id/index.ts
+++ product-service/handlers/get-product-by-id/index.ts
@@ -1,29 +1,42 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import products from '../../mocks/products';
+import { Client } from 'pg';
+import dbOptions from '../../utils/dbOptions';
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  console.log('Get product handler: ', event);
+  let client = null;
+
   try {
+    client = new Client(dbOptions);
+    await client.connect();
+
     const { productId } = event.pathParameters;
     if (!productId) {
       throw new Error('Please provide product id!');
     }
 
-    const product = products.find(item => item.id === productId);
-    if (!product) {
-      throw new Error(`Product with id='${productId}' not found!`);
+    const query = {
+      text: 'SELECT * FROM products p LEFT JOIN stocks s on p.id = s.product_id WHERE id = $1',
+      values: [productId]
+    }
+
+    const { rows } = await client.query(query);
+    if (!rows.length) {
+      throw { status: 404, message: `Product with id=${productId} not found` };
     }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(product),
+      body: JSON.stringify(rows[0]),
     };
-
   }catch (error) {
-    console.error(error);
+    console.error('Get product by id error:', error.message);
     return {
-      statusCode: 400,
+      statusCode: error.statusCode || 500,
       body: JSON.stringify(error.message),
-    }
+    };
+  }finally {
+    client.end()
   }
 }
 
