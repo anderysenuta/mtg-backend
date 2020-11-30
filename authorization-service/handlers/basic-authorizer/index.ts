0974b02204f7267c5b40c1c56ac56a6233f95869
Andrei Sianiuta <anderysenuta@gmail.com>
2020-11-30 03:10:54 +0300

    TASK_7 - init commit - add auth handler - change policy - add cors resource headers

--- authorization-service/handlers/basic-authorizer/index.ts
+++ authorization-service/handlers/basic-authorizer/index.ts
@@ -1,11 +1,44 @@
-import { S3Handler } from 'aws-lambda';
+import { APIGatewayTokenAuthorizerHandler, APIGatewayAuthorizerResult } from 'aws-lambda';
 
-export const handler: S3Handler = (event, _context) => {
+const UNAUTHORIZED = 'Unauthorized';
+
+export const handler: APIGatewayTokenAuthorizerHandler = (event, _context, callback) => {
   console.log('Basic authorizer parser: ', event);
   try {
-    console.log('init');
+
+    if (event.type !== 'TOKEN') {
+      callback(UNAUTHORIZED);
+    }
+
+    const token = event.authorizationToken.split(' ')[1];
+    const buff = Buffer.from(token, 'base64');
+    const [ username, password ] = buff.toString('utf-8').split(':');
+    const storedPassword = process.env[username];
+
+    const effect = !storedPassword || storedPassword !== password ? 'Deny' : 'Allow';
+    const policy = generatePolicy(token, event.methodArn, effect);
+
+    callback(null, policy);
+
+
   }catch (error) {
     console.error('Basic authorizer parser error:', error.message);
+    callback(`${UNAUTHORIZED} ${error.message}`);
   }
 }
 
+const generatePolicy = (principalId: string, resource: string, effect = 'Allow') : APIGatewayAuthorizerResult => {
+  return {
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource
+        }
+      ]
+    }
+  }
+}
