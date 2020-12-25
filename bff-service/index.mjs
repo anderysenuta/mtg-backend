b4830742defaf9b836c32df2b7342931e6a997f4
Andrei Sianiuta <anderysenuta@gmail.com>
2020-12-25 19:03:16 +0300

    TASK_9 add extensions

--- bff-service/index.mjs
+++ bff-service/index.mjs
@@ -1,4 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,11 +16,38 @@ app.all('/*', (req, res) => {
   console.log(req.method);
   console.log(req.body);
 
-
   const recipient = req.originalUrl.split('/')[1];
   const recipientUrl = process.env[recipient]
 
-  res.send();
+  console.log('===');
+  console.log(recipientUrl);
+
+  if (recipientUrl) {
+    const axiosConfig = {
+      method: req.method,
+      url: `${recipientUrl}${req.originalUrl}`,
+      ...(Object.keys(req.body || {}).length > 0 && { data: req.body })
+    }
+
+    axios(axiosConfig)
+      .then(response => {
+        console.log('response data from recipients', response.data);
+        res.json(response.data);
+      })
+      .catch(err => {
+        console.error('some error',err);
+
+        if (err.response) {
+          const { status, data } = err.response;
+          res.status(status).json(data);
+        } else {
+          res.status(500).json({error: err.message})
+        }
+      })
+  } else {
+    res.status(502).json({error: 'Cannot process request'})
+  }
+
 });
 
 app.listen(PORT, () => {
